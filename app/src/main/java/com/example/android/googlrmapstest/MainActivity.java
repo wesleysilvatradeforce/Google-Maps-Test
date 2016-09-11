463c32fa56dd60bb228115cbb9702a1da0318844
Wesley Lima da Silva <wesleysilva@Wesleys-MacBook-Pro.local>
2016-09-10 23:43:17 -0300

    add maps

--- app/src/main/java/com/example/android/googlrmapstest/MainActivity.java
+++ app/src/main/java/com/example/android/googlrmapstest/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.android.googlrmapstest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-34, 151);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        PolylineOptions polylineOptions = new PolylineOptions();
+        polylineOptions.color(Color.parseColor("#FF00FF"));
+        polylineOptions.width(5);
+        polylineOptions.add(sydney);
+
+        int l = 151;
+        int h = -34;
+        for(int i =0; i<100; i++){
+            if(i%5 == 0)
+                h+=10;
+            polylineOptions.add(new LatLng(h, l--));
+        }
+
+
+        googleMap.addPolyline(polylineOptions);
     }
 }
